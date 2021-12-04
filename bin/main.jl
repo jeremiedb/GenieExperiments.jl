d73b90521281e2598a7766525dcb68db3798f6d9
jeremie.db <jeremie.db@evovest.com>
2021-12-04 13:03:38 -0500

    up

--- bin/main.jl
+++ bin/main.jl
@@ -1,6 +1,12 @@
 ENV["GENIE_ENV"] = "prod"
 
+@info "pwd(),: $(pwd())"
+@info "@__DIR__,: $(@__DIR__)"
+
 pwd() == joinpath(@__DIR__, "bin") && cd(@__DIR__) # allow starting app from bin/ dir
+
+@info "pwd(),: $(pwd())"
+@info "@__DIR__,: $(@__DIR__)"
 
 using GenieExperiments
 push!(Base.modules_warned_for, Base.PkgId(GenieExperiments))
