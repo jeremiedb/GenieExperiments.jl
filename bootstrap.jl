88576938531bc76c028b1d8f067727ad9d9788f5
jeremie <jeremie.desgagne.bouchard@gmail.com>
2022-12-19 19:42:09 -0500

    experiments

--- bootstrap.jl
+++ bootstrap.jl
@@ -1,5 +1,5 @@
-pwd() == joinpath(@__DIR__, "bin") && cd(@__DIR__) # allow starting app from bin/ dir
+(pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
 
 using GenieExperiments
-push!(Base.modules_warned_for, Base.PkgId(GenieExperiments))
+const UserApp = GenieExperiments
 GenieExperiments.main()
