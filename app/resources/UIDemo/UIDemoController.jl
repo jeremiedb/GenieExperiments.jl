f75b501cbdd55b0351a34d9390b6d4c3f519caec
jeremie <jeremie.desgagne.bouchard@gmail.com>
2023-01-15 02:32:12 -0500

    vanilla ui demo

--- app/resources/UIDemo/UIDemoController.jl
+++ app/resources/UIDemo/UIDemoController.jl
@@ -7,21 +7,27 @@ using Dates
 @reactive mutable struct MyModel <: ReactiveModel
     name::R{String} = "MyName"
 
-    # select
+    # input
     input_text::R{String} = "default"
-    input_text_code::String = "some code"
+    input_text_code::String = "<q-input v-model=\"input_text\" label=\"Enter text\" />"
     input_num::R{Real} = 0
-    # input_date::R{String} = ""
-    input_date::R{Date} = Dates.today()
-    # input_date::R{Real} = ""
+    input_num_code::String = "<q-input v-model.number=\"input_num\" type=\"number\" label=\"Enter a number\" />"
 
     # select
     item::R{String} = ""
-    item::R{String} = ""
+    items::R{Vector{String}} = []
     item_options::R{Vector} = ["my", "default", "options"]
 
+    # radio - checkbox - buttons
     radio_item::R{String} = "circle"
     checkbox_item::R{Bool} = false
+    button_switch::R{Bool} = false
+    button_reset::R{Bool} = false
+
+    # dates
+    input_date::R{String} = "2022-01-21"
+    date_picker::R{String} = "2022/01/22"
+    date_picker_input::R{String} = "2022/01/23"
 
     flag::R{Bool} = false
 end
@@ -35,12 +41,11 @@ function handlers(m)
     on(m.item) do _
         m.name[] = "item was changed"
     end
-    # on(m.flag) do _
-    #     m.name[] = "bottom clicked while $(m.flag[])"
-    # end
-    # on(m.flag) do _
-    #     m.name[] = "bottom clicked while $(m.flag[])"
-    # end
+    on(m.button_reset) do _
+        if m.button_reset[]
+            m.button_reset[] = false
+        end
+    end
     return m
 end
 
