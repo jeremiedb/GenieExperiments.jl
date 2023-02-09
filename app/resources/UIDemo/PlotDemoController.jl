274d0589f674b171920b4521c8a2148f40de5bc2
jeremie <jeremie.desgagne.bouchard@gmail.com>
2023-02-09 17:53:54 -0500

    up

--- app/resources/UIDemo/PlotDemoController.jl
+++ app/resources/UIDemo/PlotDemoController.jl
@@ -18,10 +18,11 @@ PlotlyLight.Defaults.layout[].title.text = "Default title3!"
 # pltc = StipplePlotly.PlotConfig()
 
 # intent: replicate Shiny behavior: https://shiny.rstudio.com/gallery/conditionalpanel-demo.html
-# <img width="357.479553222656" height="300" alt="Plot object" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAb...
+# <img width="357.479553222656" height="300" alt="Plot object" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAb...">
 # io = IOBuffer()
 # p = Plots.scatter(rand(5), rand(5))
 # Plots.png(p, io)
+# str = String(take!(io))
 
 function write_plotly(o::Plot)
     io = IOBuffer()
@@ -35,50 +36,50 @@ function write_plotly(o::Plot)
     return plt
 end
 
-@vars MyModel begin
-    name::String = "MyName"
-    btn1::Bool = false
+# @vars MyModel begin
+#     name::String = "MyName"
+#     btn1::Bool = false
     
-    plt_1::String = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
-    plt_2::String = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
+#     plt_1::String = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
+#     plt_2::String = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
 
-    plt_1_data::PlotData = StipplePlotly.PlotData(x = sort(rand(10)), y = randn(10))
-    plt_1_layout::PlotLayout = StipplePlotly.PlotLayout()
+#     plt_1_data::PlotData = StipplePlotly.PlotData(x = sort(rand(10)), y = randn(10))
+#     plt_1_layout::PlotLayout = StipplePlotly.PlotLayout()
 
-    plt_2_data::Vector{Config} =
-        [PlotlyLight.Config(x = sort(rand(20)), y = randn(20), type = "scatter")]
-    plt_2_layout::Config = PlotlyLight.Config()
-
-    plt_3_data::Vector{Config} =
-        [PlotlyLight.Config(x = sort(rand(20)), y = randn(20), type = "scatter")]
-    plt_3_layout::Config =
-        PlotlyLight.Config(template = PlotlyLight.template("plotly_dark"))
-
-end
-
-# @reactive mutable struct MyModel <: ReactiveModel
-#     name::R{String} = "MyName"
-#     btn1::R{Bool} = false
-    
-#     plt_1::R{String} = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
-#     plt_2::R{String} = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
-
-#     plt_1_data::R{PlotData} = StipplePlotly.PlotData(x = sort(rand(10)), y = randn(10))
-#     plt_1_layout::R{PlotLayout} = StipplePlotly.PlotLayout()
-
-#     plt_2_data::R{Vector{Config}} =
+#     plt_2_data::Vector{Config} =
 #         [PlotlyLight.Config(x = sort(rand(20)), y = randn(20), type = "scatter")]
-#     plt_2_layout::R{Config} = PlotlyLight.Config()
+#     plt_2_layout::Config = PlotlyLight.Config()
 
-#     plt_3_data::R{Vector{Config}} =
+#     plt_3_data::Vector{Config} =
 #         [PlotlyLight.Config(x = sort(rand(20)), y = randn(20), type = "scatter")]
-#     plt_3_layout::R{Config} =
+#     plt_3_layout::Config =
 #         PlotlyLight.Config(template = PlotlyLight.template("plotly_dark"))
 
-#     # plt_static_1::Any = String(io.data)
-#     # plt_static_1::Any = "test"
-
 # end
+
+@reactive mutable struct MyModel <: ReactiveModel
+    name::R{String} = "MyName"
+    btn1::R{Bool} = false
+    
+    plt_1::R{String} = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
+    plt_2::R{String} = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
+
+    plt_1_data::R{PlotData} = StipplePlotly.PlotData(x = sort(rand(10)), y = randn(10))
+    plt_1_layout::R{PlotLayout} = StipplePlotly.PlotLayout()
+
+    plt_2_data::R{Vector{Config}} =
+        [PlotlyLight.Config(x = sort(rand(20)), y = randn(20), type = "scatter")]
+    plt_2_layout::R{Config} = PlotlyLight.Config()
+
+    plt_3_data::R{Vector{Config}} =
+        [PlotlyLight.Config(x = sort(rand(20)), y = randn(20), type = "scatter")]
+    plt_3_layout::R{Config} =
+        PlotlyLight.Config(template = PlotlyLight.template("plotly_dark"))
+
+    # plt_static_1::R{Any} = String(io.data)
+    plt_static_1::R{Any} = "test"
+
+end
 
 init() = Stipple.init(MyModel)
 
@@ -95,10 +96,10 @@ function handlers(m)
     on(m.btn1) do _
         m.plt_1[] = write_plotly(PlotlyLight.Plot(x = sort(rand(8)), y = randn(8)))
 
-        # io = IOBuffer()
-        # p = Plots.scatter(rand(5), rand(5))
-        # Plots.png(p, io)
-        # m.plt_static_1[] = String(take!(io))
+        io = IOBuffer()
+        p = Plots.scatter(rand(5), rand(5))
+        Plots.png(p, io)
+        m.plt_static_1[] = String(take!(io))
     end
     return m
 end
