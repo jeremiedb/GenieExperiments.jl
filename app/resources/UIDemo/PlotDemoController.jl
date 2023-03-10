a2d858facb4ee6e4e0920c6a76b451e293788b84
jeremie <jeremie.desgagne.bouchard@gmail.com>
2023-03-10 15:40:12 -0500

    up

--- app/resources/UIDemo/PlotDemoController.jl
+++ app/resources/UIDemo/PlotDemoController.jl
@@ -11,9 +11,6 @@ using Base64
 
 import Stipple: render
 
-# StipplePlotly.plot(:iris_plot_data; layout = :plot_layout, config = :plot_config)
-# pltc = StipplePlotly.PlotConfig()
-
 # intent: replicate Shiny behavior: https://shiny.rstudio.com/gallery/conditionalpanel-demo.html
 # <img width="357.479553222656" height="300" alt="Plot object" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAb...">
 p = Plots.scatter(rand(10), rand(10))
@@ -21,23 +18,6 @@ p_static_1 = stringmime("image/png", p)
 
 p = CairoMakie.scatter(randn(20, 2), color = 1:20)
 p_static_2 = stringmime("image/png", p)
-
-# f = CairoMakie.Figure()
-# ax1 = CairoMakie.Axis(f[1, 1], yticklabelcolor = :black, yaxisposition = :right)
-# ax2 = CairoMakie.Axis(f[1, 1], yticklabelcolor = :black, yaxisposition = :left)
-# CairoMakie.hidespines!(ax1)
-# CairoMakie.hidexdecorations!(ax1)
-# CairoMakie.lines!(ax2, 0:10, sin, color = :navy)
-# CairoMakie.barplot!(ax1, 0:10, rand(11) .* 100, color = "#3399CC66")
-# f
-# io = IOBuffer()
-# show(io, MIME("image/png"), p)
-# str = take!(io)
-# base64encode(str)
-# io = IOBuffer()
-# show(io, MIME("text/html"), p)
-# str = take!(io)
-# base64encode(str)
 
 @vars MyModel begin
     name::String = "MyName"
